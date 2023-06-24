35c0ad8afa5214ae05a035d73d45299ed099da0d
emmanuel <emmanuel.mukasa@moringaschool.com>
2023-06-24 23:22:36 +0300

    Asigment almost done

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+
+    const button = document.getElementById("button");
+
+    function clickAlert(){
+        alert("i was clicked!");
+        console.log("i was clicked!");
+    }
+    return button.addEventListener("click",clickAlert);
 }
+
+
+
